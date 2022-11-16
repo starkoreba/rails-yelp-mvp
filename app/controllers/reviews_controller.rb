ffd175066024e3414cccc309ff98f32750bca93f
Maeva De Campos <kulwilcoox@gmail.com>
2022-11-16 13:38:53 +0100

    done

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,36 @@
+class ReviewsController < ApplicationController
+  # We need to find the restaurant associated with the review
+  before_action :set_restaurant, only: %i[new create]
+
+  def new
+    # We need @restaurant in our `simple_form_for`
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new(review_params)
+    @review.restaurant = @restaurant
+    if @review.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @review = Review.find(params[:id])
+    @review.destroy
+    redirect_to restaurant_path(@review.restaurant), status: :see_other
+  end
+
+  private
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:restaurant_id])
+  end
+
+  def review_params
+    params.require(:review).permit(:content, :rating)
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+require 'faker'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +6,20 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+puts 'Creating 5 restaurants with 2 reviews...'
+5.times do
+  resto = Restaurant.create(
+    name: Faker::Name.name,
+    address: Faker::Address.street_address,
+    category: %w[chinese italian japanese french belgian].sample
+  )
+  next if resto.nil?
+
+  2.times do
+    resto.reviews.create(
+      content: Faker::Lorem.paragraph,
+      rating: rand(0..5)
+    )
+  end
+end
+puts 'Finished!'
